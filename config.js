b31844151d59fe7daa87342474604e2f7af67e5c
chistiyatraders32-maker <chistiyatraders32@gmail.com>
2026-09-18 17:11:25 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ektcnsozshcrphiuxypz.supabase.co";
+const SUPABASE_KEY = "sb_publishable_MtE65Q5d_YzQNc9N7ynZCQ_apV-Nam8";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
